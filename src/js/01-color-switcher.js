1a9ab5b71c042b3bc225af8013c71bfeaf558710
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-01-28 14:24:13 -0800

    js-hw-09-task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
 
+//grab elements
+const startBtn = document.querySelector('button[data-start="start"]');
+const stopBtn = document.querySelector('button[data-stop="stop"]');
+const backgroundElem = document.querySelector("body");
+let timerID = null;
+
+// random color arrow function
+// const getRandomHexColor = () => {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// };
+
+//assigns random color
+function getRandomHexColor() {        
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+ //turns start button off
+function disableBtn() {        
+    startBtn.disabled = true;
+};
+
+//turns start button on
+function enableBtn() {        
+    startBtn.disabled = false;
+};
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        const randomColor = getRandomHexColor();            
+        backgroundElem.style.backgroundColor = randomColor;
+    }, 1000);
+    disableBtn()
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    enableBtn();
+});
