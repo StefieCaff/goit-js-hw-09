a63e007cd878c2d2bd368ae70a2fb124451670b1
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-01-31 12:31:32 -0800

    working out task 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,10 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-
-const calendarInput = document.querySelector("input#datetime-picker");
+const startBtn = document.querySelector('button[data-start="start"]'); //start button
+const calendarInput = document.querySelector("input#datetime-picker"); //input field for calendar/time selection
 const fp = flatpickr(calendarInput, {});  // flatpickr
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -11,8 +12,34 @@ const options = {
   minuteIncrement: 1,
   theme: "material_green",
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+      console.log(selectedDates[0]);
+   
+    function pastDateAlert() {
+      var userDate = selectedDates[0];
+        var nowDate = new Date();
+        
+      if (nowDate > userDate) {
+          alert("Please choose a date in the future");
+        return false;
+      }
+      if (nowDate < userDate) {
+        startBtn.disabled = false;
+        return true;
+      }
+    };
+      
+    pastDateAlert()
   },
+};
+
+ //turns start button off
+function disableBtn() {        
+    startBtn.disabled = true;
+};
+
+//turns start button on
+function enableBtn() {        
+    startBtn.disabled = false;
 };
 
 function convertMs(ms) {
@@ -39,3 +66,7 @@ console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
 flatpickr(calendarInput, options);
+// startBtn.addEventListener("click", () => {
+//     convertMs(selectedDate);
+//     disableBtn(); 
+// });
