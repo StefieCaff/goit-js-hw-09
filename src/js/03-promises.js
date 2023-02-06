385478e981863e696599e2b583be7e298281c9aa
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-06 15:49:38 -0800

    testing

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,21 +3,16 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 // Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
 
-refs = {
-  firstDelay: document.getElementsByName("delay")[0],
-  step: document.getElementsByName("step")[0],
-  amount: document.getElementsByName("amount")[0],
-  createBtn: document.querySelector("button"),
+
+  const firstDelay = document.getElementsByName("delay")[0];
+  const step = document.getElementsByName("step")[0];
+  const amount = document.getElementsByName("amount")[0];
+  const createBtn = document.querySelector("button");
+
+const testingInput = () => {
+
+  console.log("hi");
+ 
 };
 
-console.log(refs.firstDelay.value);
-console.log(refs.step.value);
-console.log(refs.amount.value);
-
-function createPromise() {
-  console.log(refs.firstDelay.value);
-  console.log(refs.step.value);
-  console.log(refs.amount.value);
-};
-
-refs.createBtn.addEventListener('click', createPromise());
+createBtn.addEventListener('click', testingInput());
