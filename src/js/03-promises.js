c47f37be8c0dcf1a2478859e75663bb08816ed7d
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-14 11:59:17 -0800

    revert src problems

---
+++ src/js/03-promises.js
@@ -0,0 +1,14 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+// Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+
+
+  const firstDelay = document.getElementsByName("delay")[0];
+  const step = document.getElementsByName("step")[0];
+  const amount = document.getElementsByName("amount")[0];
+  const createBtn = document.querySelector("button");
+
+
+console.log("hi");
+console.log("world")
