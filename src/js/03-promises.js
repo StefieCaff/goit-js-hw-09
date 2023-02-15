203e1b1b0c1a814c248b02384daeaac6e858d7ca
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-15 12:46:17 -0800

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,56 +1,42 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-// Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+const form = document.querySelector('form');
 
-
-  const firstDelay = document.getElementsByName("delay")[0];
-  const step=document.getElementsByName("step")[0];
-  const amount=document.getElementsByName("amount")[0];
-  const createBtn=document.querySelector("button");
-
-
-
-console.log("world");
-
-createBtn.addEventListener('click', function (e) {
-  const delay = ms => new Promise(resolve => setTimeout(resolve, ms)); 
-
-
-
-});
-
-
-
-
-
-
-for (let i = 0; i <= 20; i += 5) {
-  console.log(i);
-};
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-};
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//function to create promise and settimeout to mimic api call
+function createPromise(delay, position) {
+  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
+};
 
-  const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+//function to loop through number of times entered into form
 
-(async function loop() {
-    for (let i = 0; i < 10; i++) {
-        await delay(Math.random() * 1000);
-        console.log(i);
-    }
-})();
+function callPromise(e) {
+  e.preventDefault();
+  let firstDelay = Number(e.target.delay.value);
+  let step = Number(e.target.step.value);
+  let amount = Number(e.target.amount.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(firstDelay, i + 1)
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    firstDelay += step;
+  };
+ 
+};
+
+// event on form submit to call promise loop
+form.addEventListener('submit', callPromise);
